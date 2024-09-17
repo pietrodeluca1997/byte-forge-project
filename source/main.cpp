42189b1ed133a09de83ee668af15906241bb1082
Pietro Romano <drum97@gmail.com>
2024-09-17 19:28:27 -0300

    Main file logging and game class abstraction

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,24 @@
-#include "core_systems/application/application.hpp"
+#include <cstdlib>
 
-int main(int argc, char *argv[])
+#include "core_systems/strings/string_extensions.hpp"
+#include "core_systems/logger/logger.hpp"
+#include "pong/pong.hpp"
+
+using BFE::CoreSystems::Strings::StringExtensions;
+using std::to_string;
+
+int main(int argc, char* argv[])
 {
-    Application application;
+    Logger::Debug("Engine started with " + to_string(argc) + " argument(s): [ " + StringExtensions::Join(argv, "-") + " ]");
+    Logger::Debug("Pong game project selected...");
 
-    application.Initialize();
-    application.Run();
-    application.Shutdown();
+	Pong pong;
 
-    return 0;
+    pong.Initialize();
+    pong.Run();
+    pong.Shutdown();
+
+    Logger::Warning("Engine terminating...");
+
+    return EXIT_SUCCESS;
 }
